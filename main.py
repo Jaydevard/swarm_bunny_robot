7369a7783720e37326db6844558df74f3527c1c9
Jaydevard <60451362+Jaydevard@users.noreply.github.com>
2021-06-19 16:16:07 -0230

    Updated gui branch with main files

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from swarm_bunny_robot.robot import Bunny
+
+# Run the main program from here
+if __name__ == "__main__":
+    sys.path.append(os.getcwd())
+    print("Hello..")
+    Bunny()
+
+
+
+
+
