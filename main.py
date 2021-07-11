1c95d1e0e1ec1e2ec64674ebdf5ae3bb0c7e3bc7
Jaydevard <60451362+Jaydevard@users.noreply.github.com>
2021-07-10 21:31:51 -0230

    Added main.py and gui.py for robot modelling

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 import sys
-from swarm_bunny_robot.robot import Bunny
+from Robot.robot import Bunny
+from Robot.robotSimulator import BunnySim
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+
+
 
 # Run the main program from here
 if __name__ == "__main__":
     sys.path.append(os.getcwd())
-    print("Hello..")
-    Bunny()
+
+
+
+
+
 
 
 
