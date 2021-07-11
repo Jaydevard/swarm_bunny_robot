17b7fd1dfc0535ef638ba03014f2b6e28b9bdd07
Alex Mills <alexmillsnl@gmail.com>
2021-07-11 15:42:00 -0230

    Removing whitespace

--- main.py
+++ main.py
@@ -5,20 +5,6 @@ from Robot.robotSimulator import BunnySim
 from kivy.app import App
 from kivy.uix.widget import Widget
 
-
-
-
-
 # Run the main program from here
 if __name__ == "__main__":
     sys.path.append(os.getcwd())
-
-
-
-
-
-
-
-
-
-
