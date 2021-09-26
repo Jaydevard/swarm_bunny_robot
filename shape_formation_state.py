1fc2cfd4e0b4c1597cd8eab33c3f98966a16a60f
Girish <girish201r7gmail.com>
2021-09-26 17:42:29 -0400

    created the class move_to_targer

--- shape_formation_state.py
+++ shape_formation_state.py
@@ -3,7 +3,7 @@ import math
 
 class Shape_Formation:
 
-    #initialize the starting position, the angle of the robot with the reference axis,and the final position
+    #initialize the starting position, the angle of the robot with the reference frame,and the final position
     def __init__(self,current_x,current_y,theta_robot,final_x,final_y):
         self.current_x = current_x
         self.current_y = current_y
@@ -11,4 +11,16 @@ class Shape_Formation:
         self.final_x = final_x
         self.final_y = final_y
 
-    def
+    def move_to_target(self):
+        velocity = 20             #20 cm/s
+        rot_vel  = 0.1            #rotational velocity in rad/s
+
+        #calculate the distance bw current location and target location
+        delta_x = self.final_x-self.current_x
+        delta_y = self.final_y-self.current_x
+        distance= math.sqrt(abs(delta_x * delta_x + delta_y * delta_y))
+
+        #calculate the angle of the distance with the reference frame
+        zetha = math.atan((self.y1 - self.y) / (self.x1 - self.x))
+
+        #rotate the robot until the angle of the robot in the reference frame (theta) equals to angle (zetha)
