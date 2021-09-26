79654c8ab499e9d77f4128d47e0e91dfd4e7890d
Girish <girish201r7gmail.com>
2021-09-26 17:28:41 -0400

    Created the shape_formation_state.py and the class to initialize the robot location

--- shape_formation_state.py
+++ shape_formation_state.py
@@ -1,1 +1,14 @@
+import numpy as np
+import math
 
+class Shape_Formation:
+
+    #initialize the starting position, the angle of the robot with the reference axis,and the final position
+    def __init__(self,current_x,current_y,theta_robot,final_x,final_y):
+        self.current_x = current_x
+        self.current_y = current_y
+        self.theta_robot = theta_robot
+        self.final_x = final_x
+        self.final_y = final_y
+
+    def
