35c96f8f74e769b57b8600d892a1033c74d947dd
Jaydevard <60451362+Jaydevard@users.noreply.github.com>
2021-10-17 18:11:01 -0400

    completed stable version for radio dongle

--- custom_widgets/radio_dongle_widget/radio_dongle_widget.py
+++ custom_widgets/radio_dongle_widget/radio_dongle_widget.py
@@ -1,4 +1,3 @@
-import asyncio
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
@@ -20,6 +19,7 @@ class RadioDongleWidget(BoxLayout, Widget, ButtonBehavior):
     def __init__(self, **kw):
         super().__init__(**kw)
         self._IMAGE_PATH = "custom_widgets\\radio_dongle_widget\\images\\"
+        self._SPINNER_INITIAL_TEXT = "[b][i]Select radio dongle[/i][/b]"
         Clock.schedule_once(self.initialize_widgets, 1)
         self._active_radio_dongle = None
 
@@ -31,8 +31,20 @@ class RadioDongleWidget(BoxLayout, Widget, ButtonBehavior):
 
     def initialize_widgets(self, *args):
         self._wifi_image.source = self._IMAGE_PATH + "wifi_not_connected.png"
+        self._spinner.text = self._SPINNER_INITIAL_TEXT
         self._connect_button.bind(on_release=self.connect_to_radio)
         self._search_button.bind(on_release=self.search_for_radio_dongles)
+        self._spinner.bind(text=self.update_on_spinner_selection)
+
+    def update_on_spinner_selection(self, *args):
+        if self._spinner.text == self._SPINNER_INITIAL_TEXT or self._active_radio_dongle is None:
+            return
+        elif self._active_radio_dongle != self._spinner.text:
+            self._connect_button.text = "[b][i]Connect[i][/b]"
+            self._wifi_image.source = self._IMAGE_PATH + "wifi_not_connected.png"
+        elif self._active_radio_dongle == self._spinner.text:
+            self._connect_button.text = "[b][i]Disconnect[i][/b]"
+            self._wifi_image.source = self._IMAGE_PATH + "wifi_connected.png"
 
     def search_for_radio_dongles(self, *args):
         self._wifi_image.source = self._IMAGE_PATH + "searching_wheel.gif"
@@ -42,13 +54,21 @@ class RadioDongleWidget(BoxLayout, Widget, ButtonBehavior):
         Clock.schedule_once(partial(self.update_radio_dongles, ['radio_1', 'radio_2']), 3)
 
     def update_radio_dongles(self, radios: list, *largs):
+        """
+        Callback for radio dongles
+        ==========
+        This must be called by the communication class
+        :param radios: list of radios available
+        :param largs: handle extra arguments
+        :return: None
+        """
         self._spinner.values = radios
         self._search_button.disabled = False
         self._search_button.text = "[b][i]Search[/i][/b]"
         self._wifi_image.source = self._IMAGE_PATH + "wifi_not_connected.png"
 
     def connect_to_radio(self, *args):
-        if self._spinner.text == "[b][i]Select radio dongle[/i][/b]":
+        if self._spinner.text == self._SPINNER_INITIAL_TEXT:
             InformationPopup(_type='e', _message="Select a radio dongle")
 
         else:
@@ -60,6 +80,8 @@ class RadioDongleWidget(BoxLayout, Widget, ButtonBehavior):
 
     def finalize_radio_connection(self, connection_established: bool, *args):
         """
+        Callback for radio connection
+        ==========
         callback function when radio connection has been established or not
         :param connection_established: True if connected, False if not connected
         :param args: handle callback arguments from kivy clock
@@ -68,7 +90,7 @@ class RadioDongleWidget(BoxLayout, Widget, ButtonBehavior):
         if connection_established:
             self._spinner.disabled = False
             self._active_radio_dongle = self._spinner.text
-            self._connect_button.text = "[b][i]Connected[i][/b]"
+            self._connect_button.text = "[b][i]Disconnect[i][/b]"
             self._wifi_image.source = self._IMAGE_PATH + "wifi_connected.png"
         else:
             InformationPopup(_type='e', _message="Connection to radio could not be established!!")
